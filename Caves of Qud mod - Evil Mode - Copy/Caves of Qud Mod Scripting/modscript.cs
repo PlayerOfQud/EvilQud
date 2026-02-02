a338538f876658c2ab23c852cc7a9f4ef6b81d96
PlayerOfQud <kirk.twyman07@gmail.com>
2026-02-02 13:02:16 -0500

    Attempting to Sync github and Steam

---
+++ Caves of Qud mod - Evil Mode - Copy/Caves of Qud Mod Scripting/modscript.cs
@@ -0,0 +1,50 @@
+
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Runtime;
+using System.Security;
+using items;
+
+//Spear Attack Event
+
+/*
+namespace ReachAttack;
+
+[Serializable]
+
+public void ReachAttack(int distance){
+    
+    Part ReachAttack = new Part();
+
+    if (Object.attackevent == true && Object.range <= distance)
+    {
+        return doDamage;
+    }
+
+
+}
+
+public void CraftMenu()
+{
+    RenderRect(User.resolutionX*0.9, User.resolutionY*0.90);
+
+
+}
+
+public void CraftMenuInput(String[] items)
+{
+    //for ...
+    Object.removeItem(Object.inventory,items[i]);
+}
+
+public void CraftMenuOutput();
+
+public String Recipe()
+{
+    //
+
+
+    String CraftedItem = "";
+    return CraftedItem;
+}
+*/
